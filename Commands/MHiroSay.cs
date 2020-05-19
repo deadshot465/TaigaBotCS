4baa00bd380b7b7c5dd9193e6e130d4b851bb4df
Chehui Chou <deadshot465@users.noreply.github.com>
2020-05-19 15:25:01 +0900

    Add base class for specialized dialogs.

--- Commands/MHiroSay.cs
+++ Commands/MHiroSay.cs
@@ -1,219 +1,21 @@
-﻿using Discord;
-using Discord.Commands;
-using System;
-using System.Collections.Generic;
+﻿using Discord.Commands;
 using System.ComponentModel;
-using System.Linq;
 using System.Threading.Tasks;
-using TaigaBotCS.Services;
 using TaigaBotCS.Utility;
 
 namespace TaigaBotCS.Commands
 {
     [Attributes.Command("mhirosay", "fun", null, new[] { "mhiro", "maturehiro", "maturehirosay" })]
-    public class MHiroSay : Dialog
+    public class MHiroSay : SpecializedDialogBase
     {
-        private enum MHiroSayError
-        {
-            LengthTooShort, BackgroundNotFound, MessageNotFound,
-            MessageTooLong, WrongCharacterSet, Cooldown,
-            PoseNotFound, ClothesNotFound, FaceNotFound
-        }
-
-        private Dictionary<string, object> _userLocalization;
-        private string _currentCharacter = string.Empty;
-
         public MHiroSay() : base()
         {
             var hiroSayText = Helper.GetLocalization("en").texts.hirosay;
             var usage = hiroSayText["usage"].ToString()
                 .Replace("{backgrounds}", string.Join(", ", _backgroundString));
 
-            TypeDescriptor.AddAttributes(typeof(MHiroSay),
+            TypeDescriptor.AddAttributes(typeof(HiroSay),
                 new Attributes.CommandAttribute("mhirosay", "fun", usage, new[] { "mhiro", "maturehiro", "maturehirosay" }));
-        }
-
-        [Command("mhirosay")]
-        [Alias("mhiro", "maturehiro", "maturehirosay")]
-        [Priority(1)]
-        public async Task MHiroSayAsync()
-            => await HandleErrorAsync(MHiroSayError.LengthTooShort, string.Empty);
-
-        [Command("mhirosay")]
-        [Alias("mhiro", "maturehiro", "maturehirosay")]
-        [Priority(3)]
-        public async Task MHiroSayAsync(string help)
-        {
-            if (string.IsNullOrEmpty(_currentCharacter)) _currentCharacter = "mhiro";
-
-            var backgroundStrings = PersistenceService.DialogBackgrounds
-                .Select(str => $"`{str}`");
-
-            if (help.Trim().ToLower() == "help")
-            {
-                var specializationInfo = await DialogService.GetSpecializationInformation("hiro");
-                var embed = new EmbedBuilder
-                {
-                    Author = new EmbedAuthorBuilder
-                    {
-                        IconUrl = Context.User.GetAvatarUrl(),
-                        Name = Context.User.Username
-                    },
-                    Color = new Color(0xff6600),
-                    Fields = new List<EmbedFieldBuilder>
-                    {
-                        {
-                            new EmbedFieldBuilder
-                            {
-                                IsInline = false,
-                                Name = "**Backgrounds**",
-                                Value = string.Join(", ", backgroundStrings)
-                            }
-                        },
-                        {
-                            new EmbedFieldBuilder
-                            {
-                                IsInline = false,
-                                Name = "**Total Available Poses (0-indexed)**",
-                                Value = specializationInfo.Count
-                            }
-                        }
-                    }
-                };
-
-                foreach (var item in specializationInfo)
-                {
-                    var clothesTitle = $"**Available Clothes for Pose {item.Key}**";
-                    var facesTitle = $"**Available Faces for Pose {item.Key}**";
-
-                    var formattedClothes = item.Value["Clothes"]
-                        .Select(str => $"`{str}`");
-
-                    var formattedFaces = item.Value["Faces"]
-                        .Select(str => $"`{str}`");
-
-                    string allClothes = string.Join(", ", formattedClothes);
-                    string allFaces = string.Join(", ", formattedFaces);
-
-                    embed.Fields.Add(new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = clothesTitle,
-                        Value = allClothes
-                    });
-
-                    embed.Fields.Add(new EmbedFieldBuilder
-                    {
-                        IsInline = false,
-                        Name = facesTitle,
-                        Value = allFaces
-                    });
-                }
-
-                await Context.Channel.SendMessageAsync("Check your DM. <:chibitaiga:697893400891883531>");
-                await Context.User.SendMessageAsync(embed: embed.Build());
-            }
-            else
-            {
-                await HandleErrorAsync(MHiroSayError.LengthTooShort, string.Empty);
-            }
-        }
-
-        [Command("mhirosay")]
-        [Alias("mhiro", "maturehiro", "maturehirosay")]
-        [Priority(9)]
-        public async Task MHiroSayAsync(int pose, string clothes, string face, [Remainder] string content)
-        {
-            _currentCharacter = "mhiro";
-            await MHiroSayAsync(PersistenceService
-                .DialogBackgrounds[_rng.Next(0, PersistenceService.DialogBackgrounds.Count)], pose, clothes, face, content);
-        }
-
-        [Command("mhirosay")]
-        [Alias("mhiro", "maturehiro", "maturehirosay")]
-        [Priority(7)]
-        public async Task MHiroSayAsync(string background, int pose, string clothes, string face, [Remainder] string content)
-        {
-            SetMemberConfig(Context.User.Id);
-            if (string.IsNullOrEmpty(_currentCharacter)) _currentCharacter = "mhiro";
-
-            if (!PersistenceService.DialogBackgrounds.Contains(background))
-            {
-                _ = HandleErrorAsync(MHiroSayError.BackgroundNotFound, background);
-                return;
-            }
-
-            if (string.IsNullOrEmpty(content.Trim()) || content.Length <= 0)
-            {
-                _ = HandleErrorAsync(MHiroSayError.MessageNotFound, string.Empty);
-                return;
-            }
-
-            if ((_japaneseRegex.IsMatch(content) && content.Length > 78) || content.Length > 120)
-            {
-                _ = HandleErrorAsync(MHiroSayError.MessageTooLong, string.Empty);
-                return;
-            }
-
-            if (_emojiRegex.IsMatch(content) ||
-                _emoteMentionsRegex.IsMatch(content) ||
-                _nonAsciiAndJapaneseRegex.IsMatch(content))
-            {
-                _ = HandleErrorAsync(MHiroSayError.WrongCharacterSet, string.Empty);
-                return;
-            }
-
-            var specializationInfo = await DialogService.GetSpecializationInformation("hiro");
-
-            if (!specializationInfo.ContainsKey(pose))
-            {
-                _ = HandleErrorAsync(MHiroSayError.PoseNotFound, string.Empty);
-                return;
-            }
-
-            if (!specializationInfo[pose]["Clothes"].Contains(clothes))
-            {
-                _ = HandleErrorAsync(MHiroSayError.ClothesNotFound, string.Empty);
-                return;
-            }
-
-            if (!specializationInfo[pose]["Faces"].Contains(face))
-            {
-                _ = HandleErrorAsync(MHiroSayError.FaceNotFound, string.Empty);
-                return;
-            }
-
-            var hiroSayErrors = _userLocalization["errors"] as Dictionary<string, object>;
-
-            try
-            {
-                var stream = await DialogService.GetDialogAsync(Context, "hiro", new SpecializedDialogObject
-                {
-                    Background = background,
-                    Clothes = clothes,
-                    Face = face,
-                    IsHiddenCharacter = _currentCharacter == "hiro" ? false : true,
-                    Pose = pose,
-                    Text = content
-                }, hiroSayErrors["cooldown"].ToString());
-
-                if (stream != null)
-                {
-                    await ReplyAsync(_userLocalization["result"].ToString());
-                    await Context.Channel.SendFileAsync(stream, "result.png");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine(ex.Message);
-
-                var msg = hiroSayErrors["generic"].ToString()
-                    .Replace("{json}", ex.Message);
-
-                await Context.Channel.SendMessageAsync(msg);
-
-                return;
-            }
         }
 
         public override void SetMemberConfig(ulong userId)
@@ -226,29 +28,36 @@ namespace TaigaBotCS.Commands
             _userLocalization = responseText.texts.hirosay;
         }
 
-        private async Task HandleErrorAsync(MHiroSayError error, string message)
+        [Command("mhirosay")]
+        [Alias("mhiro", "maturehiro", "maturehirosay")]
+        [Priority(3)]
+        public override async Task SpecializedDialogBaseAsync()
         {
-            var hiroSayErrors = _userLocalization["errors"] as Dictionary<string, object>;
+            await base.SpecializedDialogBaseAsync();
+        }
 
-            var backgroundNotFound = hiroSayErrors["background_not_found"].ToString()
-                .Replace("{background}", message)
-                .Replace("{backgrounds}", _backgroundString);
+        [Command("mhirosay")]
+        [Alias("mhiro", "maturehiro", "maturehirosay")]
+        [Priority(5)]
+        public override async Task SpecializedDialogBaseAsync(string help)
+        {
+            await SpecializedDialogBaseAsync("hiro", help);
+        }
 
-            var msg = error switch
-            {
-                MHiroSayError.BackgroundNotFound => backgroundNotFound,
-                MHiroSayError.LengthTooShort => hiroSayErrors["length_too_short"].ToString(),
-                MHiroSayError.MessageNotFound => hiroSayErrors["no_message"].ToString(),
-                MHiroSayError.MessageTooLong => hiroSayErrors["message_too_long"].ToString(),
-                MHiroSayError.WrongCharacterSet => hiroSayErrors["wrong_character_set"].ToString(),
-                MHiroSayError.PoseNotFound => hiroSayErrors["pose_not_exist"].ToString(),
-                MHiroSayError.ClothesNotFound => hiroSayErrors["clothes_not_exist"].ToString(),
-                MHiroSayError.FaceNotFound => hiroSayErrors["face_not_exist"].ToString(),
-                _ => string.Empty
-            };
+        [Command("mhirosay")]
+        [Alias("mhiro", "maturehiro", "maturehirosay")]
+        [Priority(7)]
+        public override async Task SpecializedDialogBaseAsync(int pose, string clothes, string face, [Remainder] string content)
+        {
+            await base.SpecializedDialogBaseAsync(pose, clothes, face, content);
+        }
 
-            if (!string.IsNullOrEmpty(msg))
-                await Context.Channel.SendMessageAsync(msg);
+        [Command("mhirosay")]
+        [Alias("mhiro", "maturehiro", "maturehirosay")]
+        [Priority(9)]
+        public override async Task SpecializedDialogBaseAsync(string background, int pose, string clothes, string face, [Remainder] string content)
+        {
+            await SpecializedDialogBaseAsync("hiro", true, background, pose, clothes, face, content);
         }
     }
 }
---
+++ Commands/SpecializedDialogBase.cs
@@ -0,0 +1,227 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaigaBotCS.Services;
+using TaigaBotCS.Utility;
+
+namespace TaigaBotCS.Commands
+{
+    public abstract class SpecializedDialogBase : Dialog
+    {
+        protected enum ConversationError
+        {
+            LengthTooShort, BackgroundNotFound, MessageNotFound,
+            MessageTooLong, WrongCharacterSet, Cooldown,
+            PoseNotFound, ClothesNotFound, FaceNotFound
+        }
+
+        protected Dictionary<string, object> _userLocalization;
+        protected string _currentCharacter = string.Empty;
+
+        public SpecializedDialogBase() : base()
+        {
+        }
+
+        public virtual async Task SpecializedDialogBaseAsync()
+            => await HandleErrorAsync(ConversationError.LengthTooShort, string.Empty);
+
+        public abstract Task SpecializedDialogBaseAsync(string help);
+
+        public async Task SpecializedDialogBaseAsync(string character, string help)
+        {
+            if (string.IsNullOrEmpty(_currentCharacter)) _currentCharacter = character;
+
+            var backgroundStrings = PersistenceService.DialogBackgrounds
+                .Select(str => $"`{str}`");
+
+            if (help.Trim().ToLower() == "help")
+            {
+                var specializationInfo = await DialogService.GetSpecializationInformation(character);
+                var embed = new EmbedBuilder
+                {
+                    Author = new EmbedAuthorBuilder
+                    {
+                        IconUrl = Context.User.GetAvatarUrl(),
+                        Name = Context.User.Username
+                    },
+                    Color = new Color(0xff6600),
+                    Fields = new List<EmbedFieldBuilder>
+                    {
+                        {
+                            new EmbedFieldBuilder
+                            {
+                                IsInline = false,
+                                Name = "**Backgrounds**",
+                                Value = string.Join(", ", backgroundStrings)
+                            }
+                        },
+                        {
+                            new EmbedFieldBuilder
+                            {
+                                IsInline = false,
+                                Name = "**Total Available Poses (0-indexed)**",
+                                Value = specializationInfo.Count
+                            }
+                        }
+                    }
+                };
+
+                foreach (var item in specializationInfo)
+                {
+                    var clothesTitle = $"**Available Clothes for Pose {item.Key}**";
+                    var facesTitle = $"**Available Faces for Pose {item.Key}**";
+
+                    var formattedClothes = item.Value["Clothes"]
+                        .Select(str => $"`{str}`");
+
+                    var formattedFaces = item.Value["Faces"]
+                        .Select(str => $"`{str}`");
+
+                    string allClothes = string.Join(", ", formattedClothes);
+                    string allFaces = string.Join(", ", formattedFaces);
+
+                    embed.Fields.Add(new EmbedFieldBuilder
+                    {
+                        IsInline = false,
+                        Name = clothesTitle,
+                        Value = allClothes
+                    });
+
+                    embed.Fields.Add(new EmbedFieldBuilder
+                    {
+                        IsInline = false,
+                        Name = facesTitle,
+                        Value = allFaces
+                    });
+                }
+
+                await Context.Channel.SendMessageAsync("Check your DM. <:chibitaiga:697893400891883531>");
+                await Context.User.SendMessageAsync(embed: embed.Build());
+            }
+            else
+            {
+                await HandleErrorAsync(ConversationError.LengthTooShort, string.Empty);
+            }
+        }
+
+        public virtual async Task SpecializedDialogBaseAsync(int pose, string clothes, string face, [Remainder] string content)
+            => await SpecializedDialogBaseAsync(PersistenceService
+                .DialogBackgrounds[_rng.Next(0, PersistenceService.DialogBackgrounds.Count)], pose, clothes, face, content);
+
+        public abstract Task SpecializedDialogBaseAsync(string background, int pose, string clothes, string face, [Remainder] string content);
+
+        public async Task SpecializedDialogBaseAsync(string character, bool specialized,
+            string background, int pose, string clothes, string face, [Remainder] string content)
+        {
+            SetMemberConfig(Context.User.Id);
+            if (string.IsNullOrEmpty(_currentCharacter)) _currentCharacter = character;
+
+            if (!PersistenceService.DialogBackgrounds.Contains(background))
+            {
+                _ = HandleErrorAsync(ConversationError.BackgroundNotFound, background);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(content.Trim()) || content.Length <= 0)
+            {
+                _ = HandleErrorAsync(ConversationError.MessageNotFound, string.Empty);
+                return;
+            }
+
+            if ((_japaneseRegex.IsMatch(content) && content.Length > 78) || content.Length > 120)
+            {
+                _ = HandleErrorAsync(ConversationError.MessageTooLong, string.Empty);
+                return;
+            }
+
+            if (_emojiRegex.IsMatch(content) ||
+                _emoteMentionsRegex.IsMatch(content) ||
+                _nonAsciiAndJapaneseRegex.IsMatch(content))
+            {
+                _ = HandleErrorAsync(ConversationError.WrongCharacterSet, string.Empty);
+                return;
+            }
+
+            var specializationInfo = await DialogService.GetSpecializationInformation(character);
+
+            if (!specializationInfo.ContainsKey(pose))
+            {
+                _ = HandleErrorAsync(ConversationError.PoseNotFound, string.Empty);
+                return;
+            }
+
+            if (!specializationInfo[pose]["Clothes"].Contains(clothes))
+            {
+                _ = HandleErrorAsync(ConversationError.ClothesNotFound, string.Empty);
+                return;
+            }
+
+            if (!specializationInfo[pose]["Faces"].Contains(face))
+            {
+                _ = HandleErrorAsync(ConversationError.FaceNotFound, string.Empty);
+                return;
+            }
+
+            var errors = _userLocalization["errors"] as Dictionary<string, object>;
+            
+            try
+            {
+                var stream = await DialogService.GetDialogAsync(Context, character, new SpecializedDialogObject
+                {
+                    Background = background,
+                    Clothes = clothes,
+                    Face = face,
+                    IsHiddenCharacter = specialized,
+                    Pose = pose,
+                    Text = content
+                }, errors["cooldown"].ToString());
+
+                if (stream != null)
+                {
+                    await ReplyAsync(_userLocalization["result"].ToString());
+                    await Context.Channel.SendFileAsync(stream, "result.png");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+
+                var msg = errors["generic"].ToString()
+                    .Replace("{json}", ex.Message);
+
+                await Context.Channel.SendMessageAsync(msg);
+                return;
+            }
+        }
+
+        public override abstract void SetMemberConfig(ulong userId);
+
+        protected async Task HandleErrorAsync(ConversationError error, string input)
+        {
+            var errorMsg = _userLocalization["errors"] as Dictionary<string, object>;
+
+            var backgroundNotFound = errorMsg["background_not_found"].ToString()
+                .Replace("{background}", input)
+                .Replace("{backgrounds}", _backgroundString);
+
+            var msg = error switch
+            {
+                ConversationError.BackgroundNotFound => backgroundNotFound,
+                ConversationError.LengthTooShort => errorMsg["length_too_short"].ToString(),
+                ConversationError.MessageNotFound => errorMsg["no_message"].ToString(),
+                ConversationError.MessageTooLong => errorMsg["message_too_long"].ToString(),
+                ConversationError.WrongCharacterSet => errorMsg["wrong_character_set"].ToString(),
+                ConversationError.PoseNotFound => errorMsg["pose_not_exist"].ToString(),
+                ConversationError.ClothesNotFound => errorMsg["clothes_not_exist"].ToString(),
+                ConversationError.FaceNotFound => errorMsg["face_not_exist"].ToString(),
+                _ => string.Empty
+            };
+
+            if (!string.IsNullOrEmpty(msg))
+                await Context.Channel.SendMessageAsync(msg);
+        }
+    }
+}
